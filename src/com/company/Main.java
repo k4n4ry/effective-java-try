98b180e0ecfc3223acb10f5e09dba59ebfdf10ca
‘knry0329’ <bxnxsxenathxy@gmail.com>
2019-07-27 01:10:12 +0900

    add No.02 .

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import com.company.con.company.no02.No02;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        No02.exec();
+    }
+}
---
+++ src/com/company/con/company/no02/No02.java
@@ -0,0 +1,14 @@
+package com.company.con.company.no02;
+
+public class No02 {
+
+    //builder pattern
+    public static void exec() {
+        NutritionFacts cocaCola = new NutritionFacts.Builder(240, 8)
+                .calories(100)
+                .sodium(35)
+                .carbohydrate(27)
+                .build();
+
+    }
+}
