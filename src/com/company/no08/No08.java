90149eec7b9905f89d0a758e30d1a17891cd8aaf
knry0329 <bxnxsxenathxy@gmail.com>
2019-08-03 11:21:41 +0900

    add no08 .

---
+++ src/com/company/no08/No08.java
@@ -0,0 +1,39 @@
+package com.company.no08;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.Buffer;
+
+public class No08 {
+
+    /*
+    try-finally文
+    問題点
+    ・tryとfinallyブロックの中の両方で例外が発生した場合、finallyブロックの中で発生した例外がスローさレル
+     */
+    static String firstLineOfFile(String path) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(path));
+        try {
+            return br.readLine();
+        } finally {
+            br.close();
+        }
+    }
+
+    /*
+    try-with-resource文
+    ・finallyブロックでcloseメソッドを呼び出さなくても自動でcloseをしてくれるようになる（ソースが簡潔になり可読性が向上する）
+    ・close処理内で例外が発生してもtry文の中の例外がスローされる（本来知りたいはずの例外がスローされる）
+     */
+    static String firstLineOfFileTWR(String path) throws IOException {
+        try(BufferedReader br = new BufferedReader(
+                new FileReader(path)
+        )) {
+            return br.readLine();
+        }
+    }
+
+
+}
